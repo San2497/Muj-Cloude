db2489cc0748a98686dfad724c1a8d927fd9d83a
San <129204951+San2497@users.noreply.github.com>
2026-03-19 22:35:27 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,48 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc9v74rm1suf',
+  'passwordsalt' => 'BJyTVlEAbmV8rxZo8pmvh3V+y2wNKC',
+  'secret' => 'IpA0D0dByQjgCaWn9/uFhJrMtHlUeICwtZuRlHcxU8TMt1To',
+  'trusted_domains' =>
+  array (
+    0 => 'localhost',
+    1 => 'nguyen7-wa.dev.spsejecna.net',
+    2 => '192.168.0.100',
+  ),
+  'datadirectory' => '/var/www/html/owncloud/data',
+  'overwrite.cli.url' => 'http://nguyen7-wa.dev.spsejecna.net/owncloud',
+  'overwriteprotocol' => 'http',
+  'dbtype' => 'mysql',
+  'version' => '10.16.0.0',
+  'dbname' => 'owncloud',
+  'dbconnectionstring' => '',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'owncloud',
+  'dbpassword' => 'dsaiq3245-gf',
+  'allow_user_to_change_mail_address' => '',
+  'logtimezone' => 'UTC',
+  'apps_paths' =>
+  array (
+    0 =>
+    array (
+      'path' => '/var/www/html/owncloud/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 =>
+    array (
+      'path' => '/var/www/html/owncloud/apps-external',
+      'url' => '/apps-external',
+      'writable' => true,
+    ),
+  ),
+  'installed' => true,
+  'filelocking.enabled' => false,
+  'debug' => true,
+  'session_keepalive' => true,
+  'session_lifetime' => 60 * 60 * 24,
+  'loglevel' => 2,
+  'theme' => 'cyber',
+);
